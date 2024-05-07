f2f9c428fd28de725ccff3f5fe2f8701f4afa56e
Maxim <maxim.khalamov@gmail.com>
2024-05-07 23:38:27 +0300

    v0.0.1 - first version. Available movement

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/max/IT/GameDev/Bomberman/src
-# Build directory: /home/max/IT/GameDev/Bomberman/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
